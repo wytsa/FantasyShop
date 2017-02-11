1595547bdfb06060aac8f920cf4c4e352cc952eb
ely koisu <ely.koisu@gmail.com>
2017-02-10 17:15:56 -0800

    Add project files.

---
+++ FantasyShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FantasyShop.Startup))]
+namespace FantasyShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
